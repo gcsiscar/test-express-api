d1bf4f4ca9978d135fa76150302760534dfab63f
Gilbert Christopher Siscar <gilbertchristophersiscar@gmail.com>
2023-01-23 16:28:31 +0800

    Added console message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
+    console.log("Request received")
     res.status(200).json({message: "Hello World"})
 })
 
