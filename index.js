fa040d66739a0ff295f7b7d3609864254ce36b14
Gilbert Christopher Siscar <gilbertchristophersiscar@gmail.com>
2023-01-24 16:41:21 +0800

    Log body

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 
 
 app.post("/", (req, res) => {
-    console.log("Email: " + req.body.email);
+    console.log(req.body);
     res.status(200).json({message: "Ok"});
 })
 
